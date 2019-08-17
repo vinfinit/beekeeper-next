6869d2844378e13109a4a48509de4aa09bf9b5d8
vinfinit <artemenko.vladimi@gmail.com>
2019-08-17 14:09:54 +0300

    Update styles

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,30 @@ import Layout from '../components/Layout';
 import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
 
+const galleryStyle = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  justifyContent: 'space-between',
+  listStyle: 'none',
+  padding: 0
+};
+
+const articleStyle = {
+  display: 'flex',
+  flexDirection: 'column'
+}
+
 const Index = props => (
   <Layout>
     <h1>Batman TV Shows</h1>
-    <ul>
+    <ul style={galleryStyle}>
       {props.shows.map(show => (
         <li key={show.id}>
           <Link href="/posts/[id]" as={`/posts/${show.id}`}>
-            <a>{show.name}</a>
+            <article style={articleStyle}>
+              <img src={show.image.medium} />
+              <a>{show.name}</a>
+            </article>
           </Link>
         </li>
       ))}
@@ -20,6 +36,7 @@ const Index = props => (
 Index.getInitialProps = async function() {
   const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
   const data = await res.json();
+  console.log(data);
 
   console.log(`Show data fetched. Count: ${data.length}`);
 
