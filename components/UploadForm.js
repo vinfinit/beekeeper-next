1334ef76c45a4c1d1b5780117d728cb209863811
vinfinit <artemenko.vladimi@gmail.com>
2019-09-05 18:08:14 +0300

    feat: add image upload component

--- components/UploadForm.js
+++ components/UploadForm.js
@@ -1,16 +1,55 @@
+import { Component } from 'react';
+
 const uploadStyle = {
   position: 'fixed',
   right: '1px',
   top: '5px',
 };
 
-const Upload = () => (
-  <div style={uploadStyle}>
-    <form method="POST" encType="multipart/form-data" action="/api/upload">
-      <input type="file" name="image"></input>
-      <input type="submit"></input>
-    </form>
-  </div>
-);
+class Upload extends Component {
+  constructor() {
+    super();
+    this.state = {
+      image: '',
+    }
+  }
+
+  handleFileChange = e => {
+    this.setState({
+      [e.target.name]: e.target.files[0],
+    })
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    for (let name in this.state) {
+      formData.append(name, this.state[name]);
+    }
+
+    await fetch('/api/upload', {
+      method: 'POST',
+      body: formData,
+    });
+    
+    alert('done');
+  }
+
+  render() {
+    return (
+      <div style={uploadStyle}>
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            name="image" 
+            type="file"
+            onChange={this.handleFileChange}>
+          </input>
+          <input type="submit"></input>
+        </form>
+      </div>
+    )
+  }
+};
 
 export default Upload;
